837ad3d7ab01a10300332c5fad6d5b88c52e073e
Gova <govaway@gmail.com>
2023-02-11 21:12:22 +0100

    Initial commit

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const navSlide = () => {
     const burger = document.querySelector(".burger");
     const nav = document.querySelector(".nav-links");
     const navLinks = document.querySelectorAll(".nav-links li");
-    const centerBtn = document.querySelector(".btop");
+    const container = document.querySelector(".container");
 
     // the toggle navigation
     burger.addEventListener("click", () => {
@@ -11,9 +11,9 @@ const navSlide = () => {
       navLinks.forEach((link, index) => {
         if (link.style.animation) {
           link.style.animation = "";
-          centerBtn.style.visibility = "visible";
+          container.style.visibility = "visible";
         } else {
-          centerBtn.style.visibility = "hidden";
+          container.style.visibility = "hidden";
           link.style.animation = `navLinksFade 0.5s ease forwards ${
             index / 7 + 1.5
           }s`;
