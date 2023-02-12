53348840a550a50ae25ec08c2796c79d2418cc9d
Gova <govaway@gmail.com>
2023-02-12 10:10:50 +0100

    Initial commit

--- script.js
+++ script.js
@@ -25,3 +25,22 @@ const navSlide = () => {
   };
   navSlide();      
   
+  
+        // Initialize and add the map
+        function initMap() {
+  
+          // The location of Geeksforgeeks office
+          const gfg_office = {
+              lat: 28.50231,
+              lng: 77.40548
+          };
+
+          // Create the map, centered at gfg_office
+          const map = new google.maps.Map(
+                  document.getElementById("map"), {
+
+              // Set the zoom of the map
+              zoom: 17.56,
+              center: gfg_office,
+          });
+      }
